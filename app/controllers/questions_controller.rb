0d7ddc5ccc743485b52325f9e1bead46aa1ecc24
Eryck Milano <eryckmilano@gmail.com>
2022-09-06 23:02:27 -0300

    stupidcoach

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,20 @@
+class QuestionsController < ApplicationController
+  def ask
+
+  end
+
+  def answer
+    @question = params[:question]
+    @answer = stupid_coach(@question)
+  end
+
+  def stupid_coach(your_message)
+    if your_message.downcase == "i am going to work right now!"
+      ""
+    elsif your_message.end_with?("?")
+      "Silly question, get dressed and go to work!"
+    else
+      "I don't care, get dressed and go to work!"
+    end
+  end
+end
